54e92552fe01c18da9329eb01188daedff715993
Giles Forde <S00083446@mail.itsligo.ie>
2019-06-04 15:25:53 +0100

    Added Encryption / Decryption methods, have to tailor the code to suit the requirements.

--- JWT Decryption/Program.cs
+++ JWT Decryption/Program.cs
@@ -12,6 +12,27 @@ namespace JWT_Decryption
         static void Main(string[] args)
         {
 
+            
+             
+        }
+
+        // Encode
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+
+        // Decode
+        public static string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = Convert.FromBase64String(base64EncodedData);
+            return Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+
+        // Encrypt JWT Token
+        public static void EncryptJWT()
+        {
             const string sec = "ProEMLh5e_qnzdNUQrqdHPgp";
             const string sec1 = "ProEMLh5e_qnzdNU";
             var securityKey = new SymmetricSecurityKey(Encoding.Default.GetBytes(sec));
@@ -49,21 +70,44 @@ namespace JWT_Decryption
             // If someone tries to view the JWT without validating/decrypting the token,
             // then no claims are retrieved and the token is safe guarded.
             var jwt = new JwtSecurityToken(tokenString);
-             
+        }
+        
+        // Validate/Decrypt JWT Token
+        public static void DecryptJWT()
+        {
+            const string sec = "ProEMLh5e_qnzdNUQrqdHPgp";
+            const string sec1 = "ProEMLh5e_qnzdNU";
+            var securityKey = new SymmetricSecurityKey(Encoding.Default.GetBytes(sec));
+            var securityKey1 = new SymmetricSecurityKey(Encoding.Default.GetBytes(sec1));
+
+            // This is the input JWT which we want to validate.
+            string tokenString = string.Empty;
+
+            // If we retrieve the token without decrypting the claims, we won't get any claims
+            // DO not use this jwt variable
+            var jwt = new JwtSecurityToken(tokenString);
+
+            // Verification
+            var tokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidAudiences = new string[]
+                {
+        "536481524875-glk7nibpj1q9c4184d4n3gittrt8q3mn.apps.googleusercontent.com"
+                },
+                ValidIssuers = new string[]
+                {
+        "https://accounts.google.com"
+                },
+                IssuerSigningKey = securityKey,
+                // This is the decryption key
+                TokenDecryptionKey = securityKey1
+            };
+
+            SecurityToken validatedToken;
+            var handler = new JwtSecurityTokenHandler();
+
+            handler.ValidateToken(tokenString, tokenValidationParameters, out validatedToken);
         }
 
-        // Encode
-        public static string Base64Encode(string plainText)
-        {
-            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
-            return Convert.ToBase64String(plainTextBytes);
-        }
-
-        // Decode
-        public static string Base64Decode(string base64EncodedData)
-        {
-            var base64EncodedBytes = Convert.FromBase64String(base64EncodedData);
-            return Encoding.UTF8.GetString(base64EncodedBytes);
-        }
     }
 }
